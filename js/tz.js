7dc87e109dddffa907ca3b3b999b15ab5682d830
Majus-Dev <marius.espervik@gmail.com>
2022-09-20 13:28:11 +0200

    added mor shit

--- js/tz.js
+++ js/tz.js
@@ -14,18 +14,41 @@ let zones = [
     {name: "Timmy", tz: "EST", loc: "America/Detroit"},
     {name: "Keke", tz: "PST", loc: "America/Los_Angeles"}
 ]
+
+let tzones = [
+    {name: "Hap", tz: "CET", loc: "Europe/Oslo"},
+    {name: "Cel", tz: "EST", loc: "America/Detroit"},
+    {name: "Cloud", tz: "EST", loc: "America/Detroit"},
+    {name: "Ceph", tz: "CST", loc: "America/Chicago"},
+    {name: "Scrub", tz: "CST", loc: "America/Chicago"},
+    {name: "Locke", tz: "CET", loc: "Europe/Oslo"},
+    {name: "Lila", tz: "PST", loc: "America/Los_Angeles"},
+]
+
 function loop() 
 {
     const cont = document.getElementById("mods")
-    if (!cont) return 
+    const con2 = document.getElementById("twt")
+    if (!cont || !con2) return 
     cont.innerHTML = ""
+    con2.innerHTML = ""
     for (let z of zones) {
         t = new Intl.DateTimeFormat("en-US", {timeZone: z.loc, timeStyle: "medium"}).format(new Date())
         //console.log(`${z.tz} | ${z.name}: ${t}`)
+        
         let div = document.createElement("div");
         div.classList = "mod";
-        div.innerHTML = `<span class="content-l"><p>${z.name}:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></span><span class="content-r"><p>${t} | ${z.tz}</p></span>`
+        div.innerHTML = `<span class="content-l"><p>${z.name}:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></span><span class="content-r" id="timer-${z.name}"><p>${t} | ${z.tz}</p></span>`
         cont.append(div)
+
+    }
+    for (let t of tzones) {
+        z = new Intl.DateTimeFormat("en-US", {timeZone: t.loc, timeStyle: "medium"}).format(new Date())
+
+        let div = document.createElement("div");
+        div.classList = "mod";
+        div.innerHTML = `<span class="content-l"><p>${t.name}:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></span><span class="content-r"><p>${z} | ${t.tz}</p></span>`
+        con2.append(div)
     }
 }
 function looper() {
